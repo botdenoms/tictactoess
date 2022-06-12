3a87ec26ff8262d1e2889ebeab413f09c80c56ed
botdenoms <dudubot404@gmail.com>
2022-06-12 23:18:35 +0300

    added fonts, first try on home ui and minor changes to play

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'screens.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,6 +11,41 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const CircleAvatar(
+              backgroundColor: Color(0xFF4F4242),
+            ),
+            const SizedBox(height: 20.0),
+            GestureDetector(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF4F4242),
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                ),
+                child: const Text(
+                  'Play',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.white,
+                    fontFamily: 'Imprima',
+                  ),
+                ),
+              ),
+              onTap: () {
+                // play/start a new game run
+                Navigator.of(context).push(
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const PlayScreen(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
