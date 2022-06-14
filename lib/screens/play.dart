385d6902824f14399ebb7c27366f5fe62fa5466c
botdenoms <dudubot404@gmail.com>
2022-06-14 23:46:04 +0300

    added end game dialog feature

--- lib/screens/play.dart
+++ lib/screens/play.dart
@@ -14,7 +14,7 @@ class PlayScreen extends StatefulWidget {
 }
 
 class _PlayScreenState extends State<PlayScreen> {
-  final List<String> _board = List.filled(9, "");
+  List<String> _board = List.filled(9, "");
   List<int> _options = [];
   String _current = '';
   bool _loading = false;
@@ -32,7 +32,9 @@ class _PlayScreenState extends State<PlayScreen> {
       child: Scaffold(
         body: Column(
           children: [
+            const SizedBox(height: 5.0),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
@@ -42,6 +44,7 @@ class _PlayScreenState extends State<PlayScreen> {
                   icon: const Icon(
                     Icons.arrow_back_ios_new_rounded,
                     color: Color(0xFF4F4242),
+                    size: 28.0,
                   ),
                 ),
                 const PlayerTile(index: 1),
@@ -50,6 +53,7 @@ class _PlayScreenState extends State<PlayScreen> {
                 const SizedBox(width: 10.0),
               ],
             ),
+            const SizedBox(height: 40.0),
             Expanded(
               child: GridView.builder(
                 padding: const EdgeInsets.symmetric(
@@ -96,47 +100,56 @@ class _PlayScreenState extends State<PlayScreen> {
     }
   }
 
-  void _checkEnd(List<String> board) {
+  void _checkEnd(List<String> board) async {
     /* 
     0 1 2
     3 4 5
     6 7 8
     */
+    // if all pieces a filled
+    if (_options.isEmpty) {
+      print('its a draw');
+      await _endDialog('draw');
+    }
     // first row check
     if (board[0] == board[1] && board[0] == board[2] && board[0] != "") {
       print('${board[0]} wins');
+      await _endDialog(board[0]);
     }
     // second row check
     if (board[3] == board[4] && board[3] == board[5] && board[3] != "") {
       print('${board[3]} wins');
+      await _endDialog(board[3]);
     }
     // third row check
     if (board[6] == board[7] && board[6] == board[8] && board[6] != "") {
       print('${board[6]} wins');
+      await _endDialog(board[6]);
     }
     // first column check
     if (board[0] == board[3] && board[0] == board[6] && board[0] != "") {
       print('${board[0]} wins');
+      await _endDialog(board[0]);
     }
     // second column check
     if (board[1] == board[4] && board[1] == board[7] && board[1] != "") {
       print('${board[1]} wins');
+      await _endDialog(board[1]);
     }
     // third column check
     if (board[2] == board[5] && board[2] == board[8] && board[2] != "") {
       print('${board[2]} wins');
+      await _endDialog(board[2]);
     }
     // first cross
     if (board[0] == board[4] && board[0] == board[8] && board[0] != "") {
       print('${board[0]} wins');
+      await _endDialog(board[0]);
     }
     // second cross
     if (board[2] == board[4] && board[2] == board[6] && board[2] != "") {
       print('${board[2]} wins');
-    }
-    // if all pieces a filled
-    if (_options.isEmpty) {
-      print('its a draw');
+      await _endDialog(board[2]);
     }
   }
 
@@ -166,6 +179,138 @@ class _PlayScreenState extends State<PlayScreen> {
     } else {
       _aiPlay(_board, current);
     }
+  }
+
+  Future<void> _endDialog(String outcome) async {
+    String end = 'Its A draw';
+    if (outcome == 'x') {
+      end = 'You won';
+      _humanScore += 1;
+    } else if (outcome == 'o') {
+      end = 'You Lost';
+      _aiScore += 1;
+    }
+    setState(() {
+      _options.clear();
+    });
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 30.0,
+          ),
+          backgroundColor: const Color(0xFF393131),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  end,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontFamily: 'Imprima',
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'You',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontFamily: 'Imprima',
+                      ),
+                    ),
+                    const Text(
+                      'X',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontFamily: 'Imprima',
+                      ),
+                    ),
+                    Text(
+                      _humanScore.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontFamily: 'Imprima',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Pc/A.I',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontFamily: 'Imprima',
+                      ),
+                    ),
+                    const Text(
+                      'O',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontFamily: 'Imprima',
+                      ),
+                    ),
+                    Text(
+                      _aiScore.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontFamily: 'Imprima',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _options = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+                      _moves = 0;
+                      _board = List.filled(9, "");
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.refresh_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _initGame() {
